2d8494b393c9dcf70edab94c185ce65cf847af70
bastanley1211 <77221444+bastanley1211@users.noreply.github.com>
2021-04-23 16:38:08 -0700

    Main styles change, added about and services pages without adding new content to them, added carousel, CTA

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(function () {
+  $("#carouselButton").click(function () {
+    if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+      $(".carousel").carousel("pause");
+      $("#carouselButton").children("i").removeClass("fa-pause");
+      $("#carouselButton").children("i").addClass("fa-play");
+    } else {
+      $(".carousel").carousel("cycle");
+      $("#carouselButton").children("i").removeClass("fa-play");
+      $("#carouselButton").children("i").addClass("fa-pause");
+    }
+  });
+});
+
+//Reserve Campsite click open modal
+
+$(function () {
+  $("#reserveButton").click(function () {
+    $("#reserveModal").modal("show");
+  });
+});
+
+//Login button click open modal
+
+$(function () {
+  $("#loginButton").click(function () {
+    $("#loginModal").modal("show");
+  });
+});
